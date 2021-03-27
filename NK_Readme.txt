d3484b160498d1cd46c0b8e2beb1a07cdecd14e3
Nikhil Kshirsagar <nikhil.kshirsagar@lntinfotech.com>
2021-03-27 16:13:29 +0530

    Header bar

---
+++ NK_Readme.txt
@@ -0,0 +1,16 @@
+> npx create-react-app amazon-clone-official
+> cd
+> npm start
+
+> npm i styled-components
+> npm i @material-ui/core && npm i @material-ui/icons
+
+# Reference: https://reactrouter.com/web/guides/quick-start
+> npm i react-router-dom
+
+
+Commands:
+rfce : reactFunctionalExportComponent
+
+Extensions to be installed:
+	ES7 React/Redux
---
+++ src/components/Header.js
@@ -0,0 +1,122 @@
+import React from 'react'
+import styled from 'styled-components'
+import SearchIcon from '@material-ui/icons/Search';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+
+function Header() {
+    return (
+        <Container>
+            <HeaderLogo >
+                <img src={"https://i.imgur.com/7I9Was5.png"} />
+            </HeaderLogo >
+            
+            <HeaderOptionAddress>
+                <LocationOnIcon />
+                <HeaderOption>
+                    <OptionLineOne>Hello</OptionLineOne>
+                    <OptionLineTwo>Select Your Address</OptionLineTwo>
+                </HeaderOption>
+            </ HeaderOptionAddress>
+
+            <HeaderSearch>
+                <HeaderSearchInput type='text' />
+                <HeaderSearchIconContainer>
+                    <SearchIcon />
+                </HeaderSearchIconContainer>
+            </HeaderSearch>
+
+            <HeaderNavItems>
+                <HeaderOption>
+                    <OptionLineOne>Hello Nazari</OptionLineOne>
+                    <OptionLineTwo>Account & Lists</OptionLineTwo>
+                </HeaderOption>
+                <HeaderOption>
+                    <OptionLineOne>Returns</OptionLineOne>
+                    <OptionLineTwo>& Orders</OptionLineTwo>
+                </HeaderOption>
+            </HeaderNavItems>
+
+            <HeaderOptionCart>
+                <ShoppingCartIcon />
+                <CartCount>5</CartCount>
+            </HeaderOptionCart>
+        </Container>
+    )
+}
+
+export default Header
+
+const Container = styled.div`
+    height: 60px;
+    background-color: #0F1111;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    color: white;
+`
+
+const HeaderLogo = styled.div`
+    img { 
+        width: 100px;
+        margin-left: 20px;
+    }
+`
+
+const HeaderOptionAddress = styled.div`
+    display: flex;
+    align-items: center;
+`
+const OptionLineOne = styled.div`
+    padding-left: 5px;
+`
+const OptionLineTwo = styled.div`
+    padding-left: 5px;
+    font-weight: 700;
+`
+
+const HeaderSearch = styled.div`
+    padding-left: 4px;
+    display: flex;
+    flex-grow: 1;
+    height: 40px;
+    overflow: hidden;   // *** to display the below "border-radius" 
+    border-radius: 4px;
+    background-color: white;
+    :focus-within {
+        box-shadow: 0 0 0 3px #F90;
+    }
+`
+
+const HeaderSearchInput = styled.input`
+//    border: 0;
+    flex-grow: 1;
+    :focus {
+        outline: none;
+    }
+`
+const HeaderSearchIconContainer = styled.div`
+    background-color: #febd69;
+    width: 45px;
+    color: black;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+const HeaderNavItems = styled.div`
+    display:flex;
+`
+const HeaderOption = styled.div`
+    padding: 10px 9px 10px 9px; // TRouBLe
+`
+
+const HeaderOptionCart = styled.div`
+    display: flex;
+    align-items: center;
+    padding-right: 9px;
+`
+
+const CartCount = styled.div`
+    padding-left: 1px;
+`
